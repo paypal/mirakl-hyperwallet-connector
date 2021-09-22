8022e6ea51ca2104391b1102d9a147b2c27e1b5f
Damián Pérez <47520056+damian229@users.noreply.github.com>
2021-09-22 15:19:12 +0200

    Release 4.2.1 (#8)
    LGTM

---
+++ kyc/src/test/java/com/paypal/kyc/strategies/status/impl/KYCBusinessStakeholderStatusFactoryTest.java
@@ -0,0 +1,39 @@
+package com.paypal.kyc.strategies.status.impl;
+
+import com.paypal.infrastructure.strategy.Strategy;
+import com.paypal.kyc.model.KYCBusinessStakeholderStatusNotificationBodyModel;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class KYCBusinessStakeholderStatusFactoryTest {
+
+	@InjectMocks
+	private KYCBusinessStakeholderStatusFactory testObj;
+
+	@Mock
+	private Strategy<KYCBusinessStakeholderStatusNotificationBodyModel, Optional<Void>> kycBusinessStakeholderStatusNotificationBodyModelOptionalStrategyMock;
+
+	@BeforeEach
+	void setUp() {
+		testObj = new KYCBusinessStakeholderStatusFactory(
+				Set.of(kycBusinessStakeholderStatusNotificationBodyModelOptionalStrategyMock));
+	}
+
+	@Test
+	void getStrategies_shouldReturnConverterStrategyMock() {
+		final var result = testObj.getStrategies();
+
+		assertThat(result).containsExactly(kycBusinessStakeholderStatusNotificationBodyModelOptionalStrategyMock);
+	}
+
+}
